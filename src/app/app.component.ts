515a11520c599d0edd963c5421b7f73dc72752f8
Anthony Lopez-Guerra <anthony.lopez.guerra@jbhunt.com>
2021-08-25 09:02:22 -0500

    added ngSwitch, ngIf, ngFor, services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 
 @Component({
@@ -8,4 +8,15 @@ import { Component, ViewEncapsulation } from "@angular/core";
     encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent {}
+export class AppComponent {
+    user = {id:1, name: 'Kevin', gender: 'M', ageGroup: '51+', userCode: 'M51+', reqs:{}, reqsStatus: {fruitMet: false, vegMet: false, proteinMet: false, grainMet: false}, registered: true, email: 'kevin@kevinruse.com'};
+
+    loggedIn: boolean = false;
+
+    logIn(evt) {
+        console.log(evt);
+        this.loggedIn = evt;
+    }
+
+
+}
